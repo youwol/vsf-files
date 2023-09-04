fa3aafc8ebd3d5b66d25f9132a455f7227f8fcdc
youwol-guillaume <greinisch@youwol.com>
2023-09-04 09:04:27 +0200

    :tada: auto-generated from `py-youwol` TS pipeline template

---
+++ src/auto-generated.ts
@@ -0,0 +1,128 @@
+
+const runTimeDependencies = {
+    "externals": {
+        "@youwol/cdn-client": "^1.0.2",
+        "@youwol/flux-view": "^1.0.3",
+        "rxjs": "^6.5.5"
+    },
+    "includedInBundle": {}
+}
+const externals = {
+    "@youwol/cdn-client": {
+        "commonjs": "@youwol/cdn-client",
+        "commonjs2": "@youwol/cdn-client",
+        "root": "@youwol/cdn-client_APIv1"
+    },
+    "@youwol/flux-view": {
+        "commonjs": "@youwol/flux-view",
+        "commonjs2": "@youwol/flux-view",
+        "root": "@youwol/flux-view_APIv1"
+    },
+    "rxjs": {
+        "commonjs": "rxjs",
+        "commonjs2": "rxjs",
+        "root": "rxjs_APIv6"
+    }
+}
+const exportedSymbols = {
+    "@youwol/cdn-client": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/cdn-client"
+    },
+    "@youwol/flux-view": {
+        "apiKey": "1",
+        "exportedSymbol": "@youwol/flux-view"
+    },
+    "rxjs": {
+        "apiKey": "6",
+        "exportedSymbol": "rxjs"
+    }
+}
+
+const mainEntry : {entryFile: string,loadDependencies:string[]} = {
+    "entryFile": "./index.ts",
+    "loadDependencies": [
+        "@youwol/cdn-client",
+        "@youwol/flux-view",
+        "rxjs"
+    ]
+}
+
+const secondaryEntries : {[k:string]:{entryFile: string, name: string, loadDependencies:string[]}}= {}
+
+const entries = {
+     '@youwol/vsf-files': './index.ts',
+    ...Object.values(secondaryEntries).reduce( (acc,e) => ({...acc, [`@youwol/vsf-files/${e.name}`]:e.entryFile}), {})
+}
+export const setup = {
+    name:'@youwol/vsf-files',
+        assetId:'QHlvdXdvbC92c2YtZmlsZXM=',
+    version:'0.1.0-wip',
+    shortDescription:"",
+    developerDocumentation:'https://platform.youwol.com/applications/@youwol/cdn-explorer/latest?package=@youwol/vsf-files&tab=doc',
+    npmPackage:'https://www.npmjs.com/package/@youwol/vsf-files',
+    sourceGithub:'https://github.com/youwol/vsf-files',
+    userGuide:'https://l.youwol.com/doc/@youwol/vsf-files',
+    apiVersion:'01',
+    runTimeDependencies,
+    externals,
+    exportedSymbols,
+    entries,
+    secondaryEntries,
+    getDependencySymbolExported: (module:string) => {
+        return `${exportedSymbols[module].exportedSymbol}_APIv${exportedSymbols[module].apiKey}`
+    },
+
+    installMainModule: ({cdnClient, installParameters}:{
+        cdnClient:{install:(unknown) => Promise<WindowOrWorkerGlobalScope>},
+        installParameters?
+    }) => {
+        const parameters = installParameters || {}
+        const scripts = parameters.scripts || []
+        const modules = [
+            ...(parameters.modules || []),
+            ...mainEntry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/vsf-files_APIv01`]
+        })
+    },
+    installAuxiliaryModule: ({name, cdnClient, installParameters}:{
+        name: string,
+        cdnClient:{install:(unknown) => Promise<WindowOrWorkerGlobalScope>},
+        installParameters?
+    }) => {
+        const entry = secondaryEntries[name]
+        if(!entry){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const parameters = installParameters || {}
+        const scripts = [
+            ...(parameters.scripts || []),
+            `@youwol/vsf-files#0.1.0-wip~dist/@youwol/vsf-files/${entry.name}.js`
+        ]
+        const modules = [
+            ...(parameters.modules || []),
+            ...entry.loadDependencies.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+        ]
+        return cdnClient.install({
+            ...parameters,
+            modules,
+            scripts,
+        }).then(() => {
+            return window[`@youwol/vsf-files/${entry.name}_APIv01`]
+        })
+    },
+    getCdnDependencies(name?: string){
+        if(name && !secondaryEntries[name]){
+            throw Error(`Can not find the secondary entry '${name}'. Referenced in template.py?`)
+        }
+        const deps = name ? secondaryEntries[name].loadDependencies : mainEntry.loadDependencies
+
+        return deps.map( d => `${d}#${runTimeDependencies.externals[d]}`)
+    }
+}
